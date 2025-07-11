5bed01b1da4cd935cd477c78fd9a400b87fcda21
KBA-007 <bhushanambade72@gmail.com>
2025-07-11 19:59:04 +0530

    Update videoData.js

--- videoData.js
+++ videoData.js
@@ -1,6 +1,7 @@
+// videoData.js
 const videos = [
   {
     title: "Relation & Functions 01",
-    link: "https://cdn.telegram-cdn.com/file/qW1leSWEm9W4Vdnp_qBYpJwoPyVWrTrFAyKvW-dpPoL7g4Mgq-mYXr1xC1RLEOvbAQDFbf0EeqdM7rDJeUUXrbXmFGg7Q5VxOxpNN3gf7jC7tUqt90Svn67VVPAT2IXKIK1rsbqWc6vVGW53PEzj7oH2MPH3XyAvuFz7KvXGZbiHFGD0azXv4aD2W3C19Tt1DyeuUIM-VrRL77MfGNLqpLK0izF8fAqxeXhMH2Z0_7Q5wLBZoc2IpUw.mp4"
+    url: "https://cdn.telegram-cdn.com/file/qW1leSWEm9W4Vdnp_qBYpJwoPyVWrTrFAyKvW-dpPoL7g4Mgq-mYXr1xC1RLEOvbAQDFbf0EeqdM7rDJeUUXrbXmFGg7Q5VxOxpNN3gf7jC7tUqt90Svn67VVPAT2IXKIK1rsbqWc6vVGW53PEzj7oH2MPH3XyAvuFz7KvXGZbiHFGD0azXv4aD2W3C19Tt1DyeuUIM-VrRL77MfGNLqpLK0izF8fAqxeXhMH2Z0_7Q5wLBZoc2IpUw.mp4"
   }
 ];
