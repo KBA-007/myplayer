85eef15b550afd475b91b5ff1561e2b1b55f5c4b
KBA-007 <bhushanambade72@gmail.com>
2025-07-11 20:05:53 +0530

    Update videoData.js

--- videoData.js
+++ videoData.js
@@ -1,7 +1,8 @@
-// videoData.js
 const videos = [
   {
-    title: "Relation & Functions 01",
-    url: "https://cdn.telegram-cdn.com/file/qW1leSWEm9W4Vdnp_qBYpJwoPyVWrTrFAyKvW-dpPoL7g4Mgq-mYXr1xC1RLEOvbAQDFbf0EeqdM7rDJeUUXrbXmFGg7Q5VxOxpNN3gf7jC7tUqt90Svn67VVPAT2IXKIK1rsbqWc6vVGW53PEzj7oH2MPH3XyAvuFz7KvXGZbiHFGD0azXv4aD2W3C19Tt1DyeuUIM-VrRL77MfGNLqpLK0izF8fAqxeXhMH2Z0_7Q5wLBZoc2IpUw.mp4"
+    title: "Relations and Functions – Lecture 01",
+    url: "https://kba-007.github.io/myplayer/videos/relations-01.mp4",
+    download: true,
+    tgjoin: "https://t.me/NEETx_Titan"
   }
 ];
