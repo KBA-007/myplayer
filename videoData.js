561b07fc91e3ac7e178864565aa361481c432c3d
KBA-007 <184753596+KBA-007@users.noreply.github.com>
2025-07-19 20:48:23 +0530

    Update videoData.js

--- videoData.js
+++ videoData.js
@@ -1,7 +1,7 @@
 const videos = [
   {
-    title: "Relations and Functions – Lecture 01",
-    url: "https://kba-007.github.io/myplayer/videos/relations-01.mp4",
+    title: "___",
+    url: ".mp4",
     download: true,
     tgjoin: "https://t.me/NEETx_Titan"
   }
