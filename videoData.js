485e00884154d78a8efb16d53f06ee34fa55e4af
KBA-007 <184753596+KBA-007@users.noreply.github.com>
2025-08-13 19:00:24 +0530

    Update videoData.js

--- videoData.js
+++ videoData.js
@@ -1,7 +1,7 @@
 const videos = [
   {
     title: "___",
-    url: "https://stream.pwjarvis.app/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlb0lkIjoiZmJhYWNmNDktYzgzNC00YzZlLWE3MzEtZWI3NGEzZTE0NDEyIiwiZXhwIjoxNzU1MjYyMTE2fQ.AqOqtdCspRfcDLj17mScDSNpo3jfCDC3QHWZdWZ76Hs/hls/360/main.m3u8",
+    url: ".mp4",
     download: true,
     tgjoin: "https://t.me/NEETx_Titan"
   }
