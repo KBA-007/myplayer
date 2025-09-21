dc26a175664e4447bcbf24590d87b5f6bf5a11a9
KBA-007 <184753596+KBA-007@users.noreply.github.com>
2025-09-21 18:23:20 +0530

    Update videoData.js

--- videoData.js
+++ videoData.js
@@ -1,7 +1,7 @@
 const videos = [
   {
     title: "___",
-    url: ".mp4",
+    url: "https://dish.slivcdn.com/hls/live/2020591/TEN3HD/master_3500.m3u8",
     download: true,
     tgjoin: "https://t.me/NEETx_Titan"
   }
